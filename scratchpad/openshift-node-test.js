f9354a877782c5dc726a5da77f337472e133b1b0
Jamie Jones <jbjonesjr@github.com>
2017-06-05 22:44:26 -0400

    fix the openshift eamples

--- scratchpad/openshift-node-test.js
+++ scratchpad/openshift-node-test.js
@@ -3,12 +3,12 @@ const openshiftRestClient = require('openshift-rest-client');
 const util = require('util');
 
 openshiftConfigLoader({configLocation:'openshift-config.json'}).then(config => {
-  OpenshiftRestClient(config).then(client => {
+  openshiftRestClient(config).then(client => {
     // Use the client object to find a list of projects, for example
-    // client.projects.find().then(projects => {
-    //   console.log(util.inspect(projects, false, null));
-    // });
-    // client.services.find().then(services => {
+    client.projects.find().then(projects => {
+      console.log(util.inspect(projects, false, null));
+    });
+    // Client.services.find().then(services => {
     //   console.log(util.inspect(services, false, null));
     // });
     //
